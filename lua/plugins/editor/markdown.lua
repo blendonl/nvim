683175189eea6405e6057d9ace5f6747748db33a
blendonl <blendonluta@gmail.com>
2024-10-15 16:51:24 +0200

    add ai

--- lua/plugins/editor/markdown.lua
+++ lua/plugins/editor/markdown.lua
@@ -1,7 +1,9 @@
 return {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
-		opts = {},
+		opts = {
+        file_types = { "markdown", "Avante" },
+		},
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
 		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
@@ -13,6 +15,6 @@ return {
 		init = function()
 			vim.g.mkdp_filetypes = { "markdown" }
 		end,
-		ft = { "markdown" },
+		ft = { "markdown", "Avante" },
 	},
 }
